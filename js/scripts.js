92834e7c2b7bfa49c4064715ae79d10b06801a0c
Liem <leliem28@gmail.com>
2021-03-06 01:11:44 +0700

    Upload bai 4

--- js/scripts.js
+++ js/scripts.js
@@ -50,16 +50,16 @@ document.getElementById("btnMember").onclick = function () {
 }
 
 /**
- * Bài 3:
+ * Bài 3: Số chẵn / số lẻ
  */
 
 document.getElementById("btnChanLe").onclick = function () {
     var odds = 0;
     var even = 0;
 
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var num3 = document.getElementById("num3").value;
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    var num3 = Number(document.getElementById("num3").value);
 
     if (num1 % 2 == 0) {
         even += 1;
@@ -80,4 +80,32 @@ document.getElementById("btnChanLe").onclick = function () {
     }
 
     document.getElementById("kqBai3").innerHTML = "Số chẵn: " + even + " / Số lẻ: " + odds;
+}
+
+/**
+ * Bài 4: Tam giác
+ */
+
+document.getElementById("btnTamGiac").onclick = function () {
+    var x = Number(document.getElementById("x").value);
+    var y = Number(document.getElementById("y").value);
+    var z = Number(document.getElementById("z").value);
+
+    var kq = "";
+
+    if (x < y + z && y < x + z && z < y + x) {
+        if (x == y && y == z) {
+            kq = "Đây là tam giác đều";
+        } else if (x * x + y * y == z * z || x * x + z * z == y * y || y * y + z * z == x * x) {
+            kq = "Đây là tam giác vuông";
+        } else if (x == y || y == z || x == z) {
+            kq = "Đây là tam giác cân";
+        } else {
+            kq = "Đây là tam giác thường";
+        }
+    } else {
+        kq = "Đây không phải là tam giác";
+    }
+
+    document.getElementById("kqBai4").innerHTML = kq;
 }
