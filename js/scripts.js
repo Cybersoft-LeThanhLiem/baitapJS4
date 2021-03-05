387154444feadbbb7eb3c2ef6b905f8e3a5c37f1
Liem <leliem28@gmail.com>
2021-03-06 00:14:17 +0700

    Upload bai 3

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,36 @@ document.getElementById("btnMember").onclick = function () {
     var txtMember = member.options[member.selectedIndex].text;
     document.getElementById("kqBai2").innerHTML = "Chào " + txtMember;
 }
+
+/**
+ * Bài 3:
+ */
+
+document.getElementById("btnChanLe").onclick = function () {
+    var odds = 0;
+    var even = 0;
+
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = document.getElementById("num3").value;
+
+    if (num1 % 2 == 0) {
+        even += 1;
+    } else {
+        odds += 1;
+    }
+
+    if (num2 % 2 == 0) {
+        even += 1;
+    } else {
+        odds += 1;
+    }
+
+    if (num3 % 2 == 0) {
+        even += 1;
+    } else {
+        odds += 1;
+    }
+
+    document.getElementById("kqBai3").innerHTML = "Số chẵn: " + even + " / Số lẻ: " + odds;
+}
